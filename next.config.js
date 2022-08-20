fcb500a77b79615077f0c1ea53f46e7beee2ffdc
Meheer17 <meherr17.j@gmail.com>
2022-08-21 00:57:00 +0530

    Image To S3 API Completed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   env: {
       URI: "mongodb+srv://MAHI:1705@mo.jslvt.mongodb.net/Nextjs?retryWrites=true&w=majority",
-      URL:"https://next-js-opal-xi.vercel.app/",
-      B_NAME: "website-bucket-meheer",
-      B_REGION: "ap-south-1",
-      B_A_KEY: "AKIASPC62OMHQQL46TZG",
-      B_S_KEY: "y2SE/BRdeUKxfOmKQ13YcTbQZ3UbmfQVIbufNfxS"
+      URL:"https://next-js-opal-xi.vercel.app/"
   },
   images: {
     domains: ['www.google.com'],

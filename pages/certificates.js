25a8fc4929f99e2ce1aa76c01132d07811a1e52d
Meheer <75373216+Meheer17@users.noreply.github.com>
2022-08-25 01:30:40 +0530

    Update certificates.js

--- pages/certificates.js
+++ pages/certificates.js
@@ -2,14 +2,14 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Script from 'next/script'
 
-export default function Certificates(){
+export default function Certificates({projects}){
   return (
     <>
       <Head>
-        <title>Projects</title>
+        <title>Certificates</title>
       </Head>
       
-      <h1 className="text-sky-600 text-2xl text-center">Projects</h1>
+      <h1 className="text-sky-600 text-2xl text-center">Certificates</h1>
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 place-content-evenly'>
 
       {projects.map(pr => {

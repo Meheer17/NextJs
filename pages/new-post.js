36dd29fd303e2b60bed6dcbb9288e92f6aad4a1f
Meheer17 <meherr17.j@gmail.com>
2022-08-25 22:44:57 +0530

    issue2

--- pages/new-post.js
+++ pages/new-post.js
@@ -151,6 +151,10 @@ export default function NewProject() {
                             <label className="block mb-2 text-sm font-medium text-gray-900 ">Priority Number</label>
                             <input onChange={handleChange} type="number" value={form.pri} name='pri' id="pri" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  />
                         </div>
+                        <div className="mb-6">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 ">Tags</label>
+                            <input onChange={handleTags} type="text" name='tags' id="tags" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  /> 
+                        </div>
 
                         <button type="submit" className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
                     </form>

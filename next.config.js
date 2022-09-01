ebde2077cea1d9c1c52ddf1f88a9e4e97a2c1cf0
Meheer <75373216+Meheer17@users.noreply.github.com>
2022-09-02 00:35:39 +0530

    url

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  },
   env: {
       URI: "mongodb+srv://MAHI:1705@mo.jslvt.mongodb.net/Nextjs?retryWrites=true&w=majority",
-      URL:"https://next-js-opal-xi.vercel.app/",
+      URL:"https://meheer.vercel.app/",
       B_NAME: "website-bucket-meheer",
       B_REGION: "ap-south-1",
   },

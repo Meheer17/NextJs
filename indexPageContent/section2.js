5671c2528df98b732308340a6c55b4e8b6f9b117
Meheer17 <meherr17.j@gmail.com>
2022-09-04 01:17:50 +0530

    sec1-rebuild

--- indexPageContent/section2.js
+++
@@ -1,9 +0,0 @@
-export default function Section1() {
-    return (
-        <div className='py-3 p-4 grid md:grid-cols-3 grid-cols-1 text-slate-50'>
-            <p>HELLO</p>
-            <p>HELLO</p>
-            <p>HELLO</p>
-        </div>
-    )
-}

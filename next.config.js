1cbab7589ab06d71af138634460339354ca535cf
Meheer17 <meherr17.j@gmail.com>
2022-09-12 18:28:55 +0530

    Mongodb

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   env: {
-      URI: "mongodb+srv://MAHI:1705@mo.jslvt.mongodb.net/Nextjs?retryWrites=true&w=majority",
       URL:"https://meheer.vercel.app/",
       B_NAME: "website-bucket-meheer",
       B_REGION: "ap-south-1",

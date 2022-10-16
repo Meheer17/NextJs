9a10ac602ffbb246479f379cc4b2fc05105af807
Meheer17 <meherr17.j@gmail.com>
2022-10-16 23:03:23 +0530

    updates 1

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   env: {
       URL:"https://meheer.vercel.app/",
       B_NAME: "website-bucket-meheer",
-      URI:"mongodb+srv://MAHI:Meheer17@mo.jslvt.mongodb.net/Nextjs?retryWrites=true&w=majority",
       B_REGION: "ap-south-1", 
       SESSION_SECRET:"kajbsfkajsbfasfkjbasjfbkalsnfl ansofnaslfnasjfaskj nlfasfjba sflaskf nalskfnl asflkanslfkn lasflkas lfl flkaslk"
   },

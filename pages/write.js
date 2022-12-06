88cbbf434ee71f3732acc2e0cc2820c78a2ad505
Meheer17 <meherr17.j@gmail.com>
2022-12-06 22:57:26 +0530

    Notes

---
+++ pages/write.js
@@ -0,0 +1,8 @@
+export default function Write() {
+    return (
+        <>
+            <div className="p-20"></div>
+            <a className="pt-10 bg-white" href="../write.zip" download>Download from here</a>
+        </>
+    )
+}

746f8ab8403814ca937dffcbdcc249c6114c12f6
Meheer17 <meherr17.j@gmail.com>
2023-01-07 00:27:29 +0530

    removed write

--- pages/write.js
+++
@@ -1,8 +0,0 @@
-export default function Write() {
-    return (
-        <>
-            <div className="p-20"></div>
-            <a className="pt-10 bg-white" href="../write.zip" download>Download from here</a>
-        </>
-    )
-}

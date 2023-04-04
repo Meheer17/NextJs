5977ec2f80788552c7d349728fef6f6af3177ba2
Meheer17 <meherr17.j@gmail.com>
2023-04-04 20:28:08 +0530

    Update

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,13 +1,9 @@
 const EXTERNAL_DATA_URL = 'https://meheer.vercel.app/';
 
 export default async function web({ res }) {
-  // We make an API call to gather the URLs for our site
-  const request = await fetch(`${EXTERNAL_DATA_URL}api/projects`);
-  const posts = await request.json();
-
   // We generate the XML sitemap with the posts data
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <urlset xmlns="https://meheer.vercel.app/">
      <!--We manually set the two URLs we know already-->
      <url>
        <loc>https://meheer.vercel.app/</loc>
@@ -26,10 +22,6 @@ export default async function web({ res }) {
 
   res.setHeader('Content-Type', 'text/xml');
   // we send the XML to the browser
-  res.send(sitemap);
+  res.write(sitemap);
   res.end();
-
-  return {
-    props: {},
-  };
 }
